71a87af28755b9f2b0ae01244a28ad055e211f2d
Ram Charan Thota <icherryme.0407@gmail.com>
2022-02-18 16:26:45 +1100

    simple rospy publisher is created

---
+++ unit3_practice/catkin_ws/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/rcbot/The_construct_coursed/ros_basics/unit3_practice/catkin_ws/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ unit3_practice/catkin_ws/build/my_examples_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rcbot/The_construct_coursed/ros_basics/unit3_practice/catkin_ws/src/my_examples_pkg
+# Build directory: /home/rcbot/The_construct_coursed/ros_basics/unit3_practice/catkin_ws/build/my_examples_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
