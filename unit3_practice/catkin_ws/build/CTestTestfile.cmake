f897dcad8ae6f755f29bee10522b57f36d7532f1
Ram Charan Thota <icherryme.0407@gmail.com>
2022-02-18 16:30:07 +1100

    unit3 build file are removed

--- unit3_practice/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rcbot/The_construct_coursed/ros_basics/unit3_practice/catkin_ws/src
-# Build directory: /home/rcbot/The_construct_coursed/ros_basics/unit3_practice/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_examples_pkg")
--- unit3_practice/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/rcbot/The_construct_coursed/ros_basics/unit3_practice/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
